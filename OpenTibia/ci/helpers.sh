1765bbe58cdc5d9a11c95a61349130ecf7dd670a
Rafael Leal <rafaelealdias@gmail.com>
2020-02-23 20:12:46 -0300

    Debugging circleci #99

--- OpenTibia/ci/helpers.sh
+++ OpenTibia/ci/helpers.sh
@@ -21,7 +21,6 @@ function load_license() {
 }
 
 function before_script() {
-    set -e
     set -x
     mkdir -p /root/.cache/unity3d
     mkdir -p /root/.local/share/unity3d/Unity/
@@ -40,18 +39,8 @@ function before_script() {
         fi
     fi
 
-    LICENSE="UNITY_LICENSE_CONTENT_"$UPPERCASE_BUILD_TARGET
-
-    if [ -z "${!LICENSE}" ]
-    then
-        echo "$LICENSE env var not found, using default UNITY_LICENSE_CONTENT env var"
-        LICENSE=UNITY_LICENSE_CONTENT
-    else
-        echo "Using $LICENSE env var"
-    fi
-
-    echo "Writing $LICENSE to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf"
-    echo "${!LICENSE}" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
+    echo "Writing UNITY_LICENSE_CONTENT to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf"
+    echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
     set -x
 
