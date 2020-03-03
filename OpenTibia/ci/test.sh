f7cf92ce1bace1d5c9846117c7bdf60da0cb8334
Rafael Leal <rafaelealdias@gmail.com>
2020-03-03 02:18:58 -0300

    Fix env var

--- OpenTibia/ci/test.sh
+++ OpenTibia/ci/test.sh
@@ -11,16 +11,16 @@ load_license "$license.encrypted" "$license.sha1sum" $OPENTIBIA_CRYPT_KEY
 
 try_and_continue unity3d_runner -projectPath $(pwd) -runTests -testPlatform $TEST_PLATFORM -testResults $(pwd)/$TEST_PLATFORM-results.xml -logFile /dev/stdout -batchmode
 
-UNITY_EXIT_CODE=$?
+UNITY_TEST_EXIT_CODE=$?
 
-if [ $UNITY_EXIT_CODE -eq 0 ]; then
+if [ $UNITY_TEST_EXIT_CODE -eq 0 ]; then
   echo "Run succeeded, no failures occurred";
-elif [ $UNITY_EXIT_CODE -eq 2 ]; then
+elif [ $UNITY_TEST_EXIT_CODE -eq 2 ]; then
   echo "Run succeeded, some tests failed";
-elif [ $UNITY_EXIT_CODE -eq 3 ]; then
+elif [ $UNITY_TEST_EXIT_CODE -eq 3 ]; then
   echo "Run failure (other failure)";
 else
-  echo "Unexpected exit code $UNITY_EXIT_CODE";
+  echo "Unexpected exit code $UNITY_TEST_EXIT_CODE";
 fi
 
 cat $(pwd)/$TEST_PLATFORM-results.xml | grep test-run | grep Passed
